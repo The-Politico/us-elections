4682ee08bef68fb1516300d81a25446aa7d63631
Allan James Vestal <allanjamesvestal@gmail.com>
2019-11-17 18:54:37 -0500

    Make election model imports explicit.

--- elections/models/elections/__init__.py
+++ elections/models/elections/__init__.py
@@ -1,3 +1,35 @@
 # Imports from us-elections.
 from elections.models.elections.general import GeneralElection
+from elections.models.elections.presidential_primary import (
+    DemocraticPresidentialPrimaryElection,
+)
+from elections.models.elections.presidential_primary import (
+    PresidentialPrimaryElection,
+)
+from elections.models.elections.presidential_primary import (
+    RepublicanPresidentialPrimaryElection,
+)
+from elections.models.elections.primary import DemocraticPrimaryElection
 from elections.models.elections.primary import PrimaryElection
+from elections.models.elections.primary import RepublicanPrimaryElection
+from elections.models.elections.primary_runoff import (
+    DemocraticPrimaryRunoffElection,
+)
+from elections.models.elections.primary_runoff import PrimaryRunoffElection
+from elections.models.elections.primary_runoff import (
+    RepublicanPrimaryRunoffElection,
+)
+
+
+__all__ = [
+    "DemocraticPresidentialPrimaryElection",
+    "DemocraticPrimaryElection",
+    "DemocraticPrimaryRunoffElection",
+    "GeneralElection",
+    "PresidentialPrimaryElection",
+    "PrimaryElection",
+    "PrimaryRunoffElection",
+    "RepublicanPresidentialPrimaryElection",
+    "RepublicanPrimaryElection",
+    "RepublicanPrimaryRunoffElection",
+]
