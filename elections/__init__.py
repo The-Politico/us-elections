821c59688a7786f3e3fec8760857dd24b51dda5f
Allan James Vestal <allanjamesvestal@gmail.com>
2019-11-17 19:03:51 -0500

    Roll v0.2.0.

--- elections/__init__.py
+++ elections/__init__.py
@@ -3,6 +3,6 @@ from elections.models.election_years import ElectionYear
 
 
 __appname__ = "us-elections"
-__version__ = (0, 1, 1)
+__version__ = (0, 2, 0)
 
 __all__ = ["__appname__", "__version__", "ElectionYear"]
