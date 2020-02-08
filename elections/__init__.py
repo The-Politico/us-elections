3b43d7d62c690a13911d62f52b0a7f0010973fde
Allan James Vestal <allanjamesvestal@gmail.com>
2020-02-08 18:21:23 -0500

    Finalize non-partisan primary types. Roll v0.4.4.

--- elections/__init__.py
+++ elections/__init__.py
@@ -3,6 +3,6 @@ from elections.models.election_years import ElectionYear
 
 
 __appname__ = "us-elections"
-__version__ = (0, 4, 3)
+__version__ = (0, 4, 4)
 
 __all__ = ["__appname__", "__version__", "ElectionYear"]
