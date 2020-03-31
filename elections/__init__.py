a357f0007ef8879e557721053fd0ebb33183dcbe
Allan James Vestal <allanjamesvestal@gmail.com>
2020-03-31 19:03:33 -0400

    Update 2020 election dates with all known reschedulings as of the end of March 2020. Roll v0.5.0.

--- elections/__init__.py
+++ elections/__init__.py
@@ -3,6 +3,6 @@ from elections.models.election_years import ElectionYear
 
 
 __appname__ = "us-elections"
-__version__ = (0, 4, 6)
+__version__ = (0, 5, 0)
 
 __all__ = ["__appname__", "__version__", "ElectionYear"]
