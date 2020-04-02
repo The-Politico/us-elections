68ff1d3ab2e30a6225dc5910b3b02cf263157409
Allan James Vestal <allanjamesvestal@gmail.com>
2020-04-02 00:01:15 -0400

    Correct missing data. Roll v0.5.1.

--- elections/__init__.py
+++ elections/__init__.py
@@ -3,6 +3,6 @@ from elections.models.election_years import ElectionYear
 
 
 __appname__ = "us-elections"
-__version__ = (0, 5, 0)
+__version__ = (0, 5, 1)
 
 __all__ = ["__appname__", "__version__", "ElectionYear"]
