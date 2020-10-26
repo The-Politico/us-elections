8f987cfe906381bcc7977d9e07fd8b42b43b3e1d
Allan James Vestal <allanjamesvestal@gmail.com>
2020-10-26 13:17:23 -0400

    Latest lists of House incumbents.

--- elections/__init__.py
+++ elections/__init__.py
@@ -3,6 +3,6 @@ from elections.models.election_years import ElectionYear
 
 
 __appname__ = "us-elections"
-__version__ = (0, 7, 1)
+__version__ = (0, 7, 2)
 
 __all__ = ["__appname__", "__version__", "ElectionYear"]
